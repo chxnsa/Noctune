d42588765caa81007713e8a9ddcdaa9061897d45
chxnsa <anisaandi1980@gmail.com>
2026-05-28 22:15:38 +0800

    chore: setup initial Noctune project

---
+++ app/src/main/java/com/noctune/app/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.noctune.app.utils;
+
+public class Constants {
+    public static final String BASE_URL = "https://ws.audioscrobbler.com/2.0/";
+    public static final String API_KEY = "64e2dc23deaec8d7375f079fbe3cbc62";
+    public static final String FORMAT = "json";
+}
