47271b8c9c9cc47b81d23cbd362a8c017320c262
chxnsa <anisaandi1980@gmail.com>
2026-05-30 20:54:50 +0800

    feat: add Main Activity and Explore, Faves, Home, Radio Fragment

---
+++ app/src/main/java/com/noctune/app/ui/MainActivity.java
@@ -0,0 +1,47 @@
+package com.noctune.app.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.noctune.app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Load fragment pertama saat app dibuka
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (id == R.id.nav_explore) {
+                selectedFragment = new ExploreFragment();
+            } else if (id == R.id.nav_faves) {
+                selectedFragment = new FavesFragment();
+            } else if (id == R.id.nav_radio) {
+                selectedFragment = new RadioFragment();
+            }
+
+            return loadFragment(selectedFragment);
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
